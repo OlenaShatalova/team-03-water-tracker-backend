35f0de51ff1016afa7b18138414e11d39124407a
unknown <badsnake1@gmail.com>
2025-01-31 23:15:39 +0200

    added local saving photo

--- src/routers/userRoute.js
+++ src/routers/userRoute.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { getCurrentUser, updateCurrentUser } from '../controllers/userController.js';
+import {
+  getCurrentUser,
+  updateCurrentUser,
+  updateUserAvatar,
+} from '../controllers/userController.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { updateUserDataSchema } from '../validation/auth.js';
+import { upload } from '../middlewares/upload.js';
 
 export const userRouter = Router();
 
@@ -14,4 +19,10 @@ userRouter.patch(
   authenticate,
   validateBody(updateUserDataSchema),
   ctrlWrapper(updateCurrentUser),
+);
+userRouter.put(
+  '/current/avatar',
+  authenticate,
+  upload.single('avatar'),
+  ctrlWrapper(updateUserAvatar),
 );

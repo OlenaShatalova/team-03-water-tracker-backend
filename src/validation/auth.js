a80ea96e34876c1ed2345840ba397cda4f2c7959
Olena Shatalova <164022674+OlenaShatalova@users.noreply.github.com>
2025-01-31 16:05:28 +0200

    Update auth.js

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,14 +1,14 @@
 import Joi from "joi";
 
-import { EmailRegexp } from "../constants/user.js";
+import { EMAIL_REGEXP } from "../constants/user.js";
 
-export const AuthRegisterSchema = Joi.object({
+export const authRegisterSchema = Joi.object({
     name: Joi.string(),
-    email: Joi.string().pattern(EmailRegexp).required(),
+    email: Joi.string().pattern(EMAIL_REGEXP).required(),
     password: Joi.string().min(6).max(64).required(),
 });
 
-export const AuthLoginSchema = Joi.object({
-    email: Joi.string().pattern(EmailRegexp).required(),
+export const authLoginSchema = Joi.object({
+    email: Joi.string().pattern(EMAIL_REGEXP).required(),
     password: Joi.string().min(6).max(64).required(),
 });

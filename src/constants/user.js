e23990db91a380ad1b26a7f92737e6c0c15e27c2
Yuliia <164753475+Yuliia-Tyzhai@users.noreply.github.com>
2025-01-31 15:15:21 +0200

    Update user.js
    Add fixing constants

--- src/constants/user.js
+++ src/constants/user.js
@@ -1,5 +1,5 @@
-export const EmailRegexp = /^\w+([._]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+export const EMAIL_REGEXP = /^\w+([._]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-export const AccessTokenLifetime = 1000 *  60 * 15;
+export const ACCESS_TOKEN_LIFETIME = 1000 *  60 * 15;
 
-export const RefreshTokenLifetime = 1000 * 60 * 24 *30;
+export const REFRESH_TOKEN_LIFETIME = 1000 * 60 * 24 *30;

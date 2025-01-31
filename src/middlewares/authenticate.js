43b9ba5fc6f8bd3353768f07f5d2469b1138c8c7
Yuliia <164753475+Yuliia-Tyzhai@users.noreply.github.com>
2025-01-31 15:27:41 +0200

    Update authenticate.js
    add fixing authenticate middleware

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -2,27 +2,27 @@ import createHttpError from "http-errors";
 
 import { getSession, getUser } from "../services/auth.js";
 
-export const Authenticate = async (req, res, next) => {
-    const AuthHeader = req.get("Authorization");
-    if(!AuthHeader) {
+export const authenticate = async (req, res, next) => {
+    const authHeader = req.get("Authorization");
+    if(!authHeader) {
         return next(createHttpError(401, "Authorization header not found"));
     }
 
-    const [Bearer, AccessToken] = AuthHeader.split(" ");
-    if(Bearer !== "Bearer") {
+    const [bearer, AccessToken] = AuthHeader.split(" ");
+    if(bearer !== "Bearer") {
         return next(createHttpError(401, "Header must be Bearer type"));
 }
 
-const Session = await getSession({AccessToken});
-if(!Session) {
+const session = await getSession({AccessToken});
+if(!session) {
     return next(createHttpError(401, "Session not found"));
 }
 
-if(Date.now() > Session.AccessTokenValidUntil) {
+if(Date.now() > session.accessTokenValidUntil) {
     return next(createHttpError(401, "Access token expired"));
 }
 
-const User = await getUser({_id: Session.userId});
+const user = await getUser({_id: session.userId});
     if(!User) {
         return next(createHttpError(401, "User not found"));
     }

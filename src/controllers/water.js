2c57085979ca499e8a5fd5555c59780e2dc6292b
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2025-02-01 23:39:07 +0200

    add initial files water rate

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -1,5 +1,8 @@
 import { todayWater } from '../services/water.js';
 
+import createHttpError from 'http-errors';
+import { updateUserService } from '../services/auth.js';
+
 export const todayWaterController = async (req, res) => {
   const userId = req.user._id;
   const data = await todayWater({ userId });
@@ -9,4 +12,27 @@ export const todayWaterController = async (req, res) => {
     data, //data.todayRecord - повертає список всіх записів споживання води користувачем за поточний день.
     //data.percentTodayWater - повертає кількість спожитої води від денної норми користувача у процентах.
   });
+};
+
+export const updateWaterRate = async (req, res, next) => {
+  try {
+    const { _id } = req.user; // Отримуємо ID користувача з req.user
+    const { dailyNorm } = req.body; // Отримуємо dailyNorm з тіла запиту
+
+    // Оновлюємо dailyNorm у базі даних
+    const updatedUser = await updateUserService({ _id }, { dailyNorm });
+
+    if (!updatedUser) {
+      throw createHttpError(404, 'User not found');
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        dailyNorm: updatedUser.dailyNorm,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- src/routers/water.js
+++ src/routers/water.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { todayWaterController } from '../controllers/water.js';
+import { todayWaterController, updateWaterRate } from '../controllers/water.js';
+
+import { validateBody } from '../middlewares/validateBody.js';
+import { updateWaterRateSchema } from '../validation/water.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const waterRouter = Router();
 
 waterRouter.get('/water-today', ctrlWrapper(todayWaterController));
+
+waterRouter.patch(
+  '/water-rate',
+  authenticate, // Перевірка авторизації
+  validateBody(updateWaterRateSchema), // Валідація тіла запиту
+  ctrlWrapper(updateWaterRate), // Контролер
+);
 
 export default waterRouter;
